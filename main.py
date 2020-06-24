b09c5238407c374c64a2ea5c471e883e99df1fd9
Ilia Parunashvili <iparunashvili123@gmail.com>
2020-06-24 10:53:11 -0400

    flask static files

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello world!'
+@app.route('/tweet')
+def tweet():
+    return render_template('tweet.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ python-tweet.py
@@ -0,0 +1,23 @@
+import tweepy
+import os
+
+consumer_key = os.environ['CONSUMER_KEY']
+consumer_secret = os.environ['CONSUMER_SECRET']
+access_token = os.environ['ACCESS_TOKEN']
+access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+
+def OAuth():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    return auth
+
+oauth = OAuth()
+api = tweepy.API(oauth)
+
+tweet = raw_input('Tweet: ')
+check = raw_input('Are you sure you want to tweet "' + tweet + '"(y/N)')
+if check == 'y':
+    api.update_status(tweet)
+    print('a tweet has been posted')
+else:
+    print('your tweet has been cancelled')
