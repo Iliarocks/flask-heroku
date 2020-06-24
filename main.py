5d9115d4d5f435755188ce6dbbbb2cf6f9b32524
Ilia Parunashvili <iparunashvili123@gmail.com>
2020-06-24 10:21:52 -0400

    new path

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello world'
+@app.route('/<path:path>')
+def index(path):
+    return send_from_directory("static", path)
 
 if __name__ == '__main__':
     app.run(debug=True)
