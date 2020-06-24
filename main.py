3a4b4c47fd48e701e7a21f741dac77d53f78d60e
Ilia Parunashvili <iparunashvili123@gmail.com>
2020-06-24 10:26:11 -0400

    new path

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/static/<path:path>')
+@app.route('/tweet-python/<path:path>')
 def index(path):
-    return send_from_directory("static", path)
+    return send_from_directory("tweet-python", path)
 
 if __name__ == '__main__':
     app.run(debug=True)
